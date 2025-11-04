9e2ae819f32c71338759e238d37e1932a710e16a
Samuel Legrain <samuel.legrain@bstorm.be>
2025-11-04 12:02:41 +0100

    Add Disconnected Mode

--- DemoADO/Program.cs
+++ DemoADO/Program.cs
@@ -2,55 +2,74 @@
 using DemoADO.models;
 using DemoADO.Repositories;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
-string connectionString = "Data Source=desktop-hk4b100\\dataviz;Initial Catalog=ADO;Integrated Security=True;Trust Server Certificate=True";
+string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ADO;Integrated Security=True";
 
 SqlConnection connection = new SqlConnection(connectionString);
 
-// Vérifier que la connection est établie
-using (SqlConnection connection1 = connection)
+#region Code Robin
+//// Vérifier que la connection est établie
+//using (SqlConnection connection1 = connection)
+//{
+//    connection1.Open();
+
+//    Console.WriteLine("Connection réussie");
+
+//    connection1.Close();
+//}
+
+//SectionRepository sectionRepository = new SectionRepository(
+//    new SqlConnection(connectionString));
+
+//List<Section> sections = sectionRepository.GetAll();
+//foreach(Section section in sections)
+//{
+//    Console.WriteLine($"{section.Id}: {section.SectionName}");
+//}
+
+//Section? oneSection = sectionRepository.GetOneById(1010);
+//if (oneSection is not null)
+//{
+//    Console.WriteLine($"Voici la section 1010: {oneSection!.SectionName}");
+//}
+//else
+//{
+//    Console.WriteLine("Il n'y a pas de section 1010");
+//}
+
+//sections = sectionRepository.GetAllByName("Tourisme");
+//foreach (Section section in sections)
+//{
+//    Console.WriteLine($"{section.Id}: {section.SectionName}");
+//}
+//Console.WriteLine("---Avant insertion---");
+//Console.WriteLine(sectionRepository.Insert(1220, "Gestion du temps libre"));
+
+//Console.WriteLine("---Après insertion---");
+
+//sections = sectionRepository.GetAll();
+//foreach (Section section in sections)
+//{
+//    Console.WriteLine($"{section.Id}: {section.SectionName}");
+//}
+
+//Console.WriteLine("---Suppression---");
+//sectionRepository.Delete(1230); 
+#endregion
+
+SectionRepositoryDisconnected repository = new SectionRepositoryDisconnected(connection);
+
+repository.Get();
+
+foreach (DataRow row in repository.Section.Rows)
 {
-    connection1.Open();
-
-    Console.WriteLine("Connection réussie");
-
-    connection1.Close();
+    Console.WriteLine($"{row["Id"]} - {row["SectionName"]} - {row["ReceptionDate"]}");
+    if ((int)row["Id"] == 1010) {
+        row.BeginEdit();
+        row["SectionName"] = "ADO .net";
+        row.EndEdit();
+    }
 }
 
-SectionRepository sectionRepository = new SectionRepository(
-    new SqlConnection(connectionString));
-
-List<Section> sections = sectionRepository.GetAll();
-foreach(Section section in sections)
-{
-    Console.WriteLine($"{section.Id}: {section.SectionName}");
-}
-
-Section? oneSection = sectionRepository.GetOneById(1010);
-if (oneSection is not null)
-{
-    Console.WriteLine($"Voici la section 1010: {oneSection!.SectionName}");
-}
-else
-{
-    Console.WriteLine("Il n'y a pas de section 1010");
-}
-
-sections = sectionRepository.GetAllByName("Tourisme");
-foreach (Section section in sections)
-{
-    Console.WriteLine($"{section.Id}: {section.SectionName}");
-}
-Console.WriteLine("---Avant insertion---");
-Console.WriteLine(sectionRepository.Insert(1220, "Gestion du temps libre"));
-
-Console.WriteLine("---Après insertion---");
-
-sections = sectionRepository.GetAll();
-foreach (Section section in sections)
-{
-    Console.WriteLine($"{section.Id}: {section.SectionName}");
-}
-
-Console.WriteLine("---Suppression---");
-sectionRepository.Delete(1230);
+repository.Update();
---
+++ DemoADO/Repositories/SectionRepositoryDisconnected.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoADO.Repositories
+{
+    public class SectionRepositoryDisconnected
+    {
+        private SqlConnection _connection;
+
+        public DataTable Section { get; set; } = new DataTable();
+        public DateTime? lastReception { get; private set; }
+
+        private SqlDataAdapter _adapter = new SqlDataAdapter();
+
+        public SectionRepositoryDisconnected(SqlConnection connection)
+        {
+            //Liaison de la connection
+            _connection = connection;
+
+            //Définition de la commande de récupération
+            SqlCommand selectCommand = _connection.CreateCommand();
+            selectCommand.CommandText = "SELECT * FROM [Section]";
+
+            //Définition de la commande de mise à jour
+            SqlCommand updateCommand = _connection.CreateCommand();
+            updateCommand.CommandText = "UPDATE [Section] "
+                + "SET [SectionName] = @sectionName "
+                + "WHERE [Id] = @id";
+            updateCommand.Parameters.Add(new SqlParameter("sectionName",SqlDbType.VarChar,50, "SectionName"));
+            updateCommand.Parameters.Add(new SqlParameter("id",SqlDbType.Int, 0, "Id"));
+
+            //Affectation des commandes à notre adapteur
+            _adapter.SelectCommand = selectCommand;
+            _adapter.UpdateCommand = updateCommand;
+        }
+
+        public void Get()
+        {
+            _adapter.Fill(Section);
+            lastReception = DateTime.Now;
+        }
+
+        public void Update() 
+        { 
+            _adapter.Update(Section);
+        }
+    }
+}
